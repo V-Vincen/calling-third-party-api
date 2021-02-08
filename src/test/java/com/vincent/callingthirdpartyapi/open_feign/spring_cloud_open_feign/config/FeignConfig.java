49563f0ea8a60aa35b0cbf937d96a7d4b62ce93d
Vincent <601521821@qq.com>
2021-02-08 17:30:09 +0800

    update

--- src/test/java/com/vincent/callingthirdpartyapi/open_feign/spring_cloud_open_feign/config/FeignConfig.java
+++ src/test/java/com/vincent/callingthirdpartyapi/open_feign/spring_cloud_open_feign/config/FeignConfig.java
@@ -16,6 +16,13 @@ import java.io.InputStream;
 
 @Configuration
 public class FeignConfig {
+    /**
+     * doNotCloseAfterDecode(): 该构造方法的作用为，当响应返回的 response 进行解码器解析后，对其不进行关闭。
+     * 这里主要用于文件下载时，以流的形式成功返回后，防止流的关闭。
+     * 如果不设置该值，在调用第三方的下载接口时，会抛出 java.io.IOException: stream is closed 异常。
+     *
+     * @return Feign.Builder
+     */
     @Bean
     public Feign.Builder doNotCloseAfterDecode() {
         return Feign.builder().doNotCloseAfterDecode();
@@ -24,11 +31,26 @@ public class FeignConfig {
     @Autowired
     private ObjectFactory<HttpMessageConverters> messageConverters;
 
+    /**
+     * 编码器: 支持以下三种请求格式
+     * 1. application/json
+     * 2. application/x-www-form-urlencoded
+     * 3. multipart/form-data
+     *
+     * @return Encoder
+     */
     @Bean
     public Encoder feignFormEncoder() {
         return new SpringFormEncoder(new SpringEncoder(messageConverters));
     }
 
+    /**
+     * 解码器: 支持两种响应格式
+     * 1. 返回值类型为 InputStream（主要用于文件下载）
+     * 2. 其余返回值类型，以 json 数据格式进行解析
+     *
+     * @return Decoder
+     */
     @Bean
     public Decoder feignFormDecoder() {
         return (response, type) -> {
